f022a1e12153c96323f39a22fbcb1a583168a0a6
Pedro P Pimentel <pedrop.pimentel@hotmail.com>
2020-10-04 23:28:26 -0300

    login screen

--- GUI/src/app/autenticacao/login/login.module.ts
+++ GUI/src/app/autenticacao/login/login.module.ts
@@ -1,8 +1,18 @@
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { LoginComponent, LogarComponent } from './components';
+
 
 @NgModule({
   declarations: [
@@ -11,7 +21,16 @@ import { LoginComponent, LogarComponent } from './components';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatSnackBarModule,
+    FlexLayoutModule
   ]
 })
 export class LoginModule { }
